949dbdfbd4e2ef486e37fad450c882c107608464
crnicavic <cavic5000@tutanota.com>
2025-02-08 12:44:53 +0100

    fix non_homogenous function

--- nn.py
+++ nn.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.datasets import load_digits
 from abc import ABC, abstractmethod
 
 class ActivationFunction(ABC):
@@ -33,16 +35,13 @@ def create_weights(layer_sizes):
 	return w
 
 
-# NOTE: return a value if nothing is working
 def non_homogenous_add(a, b):
-	for _a, _b in zip(a, b):
-		_a = np.add(_a, _b)
-
+    for i in range(len(a)):
+        a[i] = np.add(a[i], b[i])
 
 def non_homogenous_divide(a, b):
-	for _a in a:
-		_a = np.divide(_a, b)
-
+    for i in range(len(a)):
+        a[i] = np.divide(a[i], b)
 
 class Network:
 	def __init__(self, layer_sizes, activation_f, lr=0.1):
@@ -60,16 +59,17 @@ class Network:
 	def feedforward(self, input_data):
 		# activation function to first layer
 		for i in range(self.layer_sizes[0]):
-			self.z[0][i] = self.activation.f(input_data[i])
+			self.z[0][i] = input_data[i]
+			self.a[0][i] = self.activation.f(self.z[0][i])
 
 		for l in range(len(self.weights)):
 			# getting fancy
-			self.z[l+1] = np.dot(self.weights[l], self.z[l])
+			self.z[l+1] = np.dot(self.weights[l], self.a[l])
 
 			# apply activation function
 			for i in range(len(self.z[l+1])):
 				self.a[l+1][i] = self.activation.f(self.z[l+1][i])
-
+			
 
 	def backprop(self, output):
 		# ls - neuron count in each layer
@@ -78,9 +78,11 @@ class Network:
 		# calculate delta error for last layer
 		for i in range(self.layer_sizes[-1]):
 			if output != i:
-				delta[-1][i] = self.a[-1][i]
+				delta[-1][i] = -self.a[-1][i]
 			else:
-				delta[-1][i] = self.a[-1][i] - 1
+				delta[-1][i] = 1 - self.a[-1][i]
+
+			print(delta[-1][i])
 
 			delta[-1][i] *= self.activation.df(self.z[-1][i])
 		
@@ -91,7 +93,6 @@ class Network:
 				delta[l][i] *= self.activation.df(self.z[l][i])
 
 		return delta
-
 
 	def gradient(self, output):
 		# storing derivatives of the cost by weights
@@ -121,35 +122,43 @@ class Network:
 					return
 				self.feedforward(inputs[k])
 				_dw, _db = self.gradient(outputs[k])
+
+				if k == 50:
+					print(_db)
 				
 				non_homogenous_add(dw, _dw)
 				non_homogenous_add(db, _db)
 
-			non_homogenous_divide(dw, batchsize*20)
-			non_homogenous_divide(db, batchsize*20)
+			non_homogenous_divide(dw, batchsize)
+			non_homogenous_divide(db, batchsize)
 			non_homogenous_add(self.weights, dw)
 			non_homogenous_add(self.b, db)
 
 
 	def test(self, inputs, outputs):
 		correct_count = 0
+		print(len(inputs))
 		for i in range(len(inputs)):
 			self.feedforward(inputs[i])
 
-			if outputs[i] == np.argmax(self.z[-1]):
+			if outputs[i] == np.argmax(self.a[-1]):
 				correct_count += 1
 
 		acc = correct_count / len(inputs) * 100
 		print("Accuracy: %.2f" % acc)
+		print(correct_count)
+		print(self.weights)
 
 
-data = pd.read_csv("heart.csv")
-y = data.iloc[:, -1]
-x = data.iloc[:, :-1]
-
+#data = pd.read_csv("heart.csv")
+#scaler = MinMaxScaler()
+data = load_digits()
+#y = data.iloc[:, -1]
+#x = data.iloc[:, :-1]
+sizes = [len(x.iloc[0]), 5, y.nunique()]
+x = scaler.fit_transform(x)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, shuffle=False) 
 
-sizes = [len(x.iloc[0]), 40, 40, 40, y.nunique()]
 net = Network(sizes, Sigmoid())
-net.train(x_train.to_numpy(), y_train.to_numpy(), 5)
-net.test(x_test.to_numpy(), y_test.to_numpy())
+net.train(x_train, y_train.to_numpy(), 5)
+net.test(x_test, y_test.to_numpy())
