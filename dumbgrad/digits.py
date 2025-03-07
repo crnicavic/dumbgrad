dc019a01b1c2296524bffd664c5719faa94f160a
crnicavic <cavic5000@tutanota.com>
2025-03-07 16:20:23 +0100

    this is learning but it's insanely slow

---
+++ dumbgrad/digits.py
@@ -0,0 +1,16 @@
+from graph import draw_dot
+from engine import Value
+from nn import Network
+import numpy as np
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_digits
+
+x, y = load_digits(return_X_y=True)
+x = x / 16		#normalize
+num_classes = len(np.unique(y))
+y = to_categorical(y, num_classes=num_classes)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False) 
+
+n = Network(len(x[0]), [10, 10, num_classes])
+n.train(x_train, y_train, epochs=1)
