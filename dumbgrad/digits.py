4fd1d41b78ce1a917679ff6a2f985a5d221cf130
crnicavic <cavic5000@tutanota.com>
2025-03-09 11:36:18 +0100

    digits network test

--- dumbgrad/digits.py
+++ dumbgrad/digits.py
@@ -13,5 +13,5 @@ y = to_categorical(y, num_classes=num_classes)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False) 
 
 n = Network(len(x[0]), [30, 30, num_classes])
-n.train(x_train, y_train, epochs=50)
+n.train(x_train, y_train, epochs=150, lr=0.01)
 n.test(x_test, y_test)
