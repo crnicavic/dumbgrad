fix: make_topo() now creates an array that backprops properly
For more info check the topology tests

dumbgrad/engine.py
@@ -2,103 +2,121 @@ import numpy as np
 from dumbgrad.graph import draw_dot
 
 class Value:
-	def __init__(self, data, op=None, children=[], label=''):
-		self.data = data
-		self.grad = 0 # what is the derrivative of the output by this variable
-		self.children = children
-		self.op = op
-		self.label = label
-		self.m = 0
-		self.v = 0
+    def __init__(self, data, op=None, children=[], label=''):
+        self.data = data
+        self.grad = 0 # what is the derrivative of the output by this variable
+        self.parents = []
+        self.children = children
+        self.op = op
+        self.label = label
+        self.m = 0
+        self.v = 0
+        for c in children:
+            c.parents.append(self)
 
-	def tanh(self):
-		out = Value(np.tanh(self.data), 'tanh', children=[self])
-		return out
+    def tanh(self):
+        out = Value(np.tanh(self.data), 'tanh', children=[self])
+        return out
 
 
-	def __add__(self, number):
-		number = number if isinstance(number, Value) else Value(number)
-		out = Value(self.data + number.data, '+', children=[self, number])
-		return out
+    def __add__(self, number):
+        number = number if isinstance(number, Value) else Value(number)
+        out = Value(self.data + number.data, '+', children=[self, number])
+        return out
 
-	def __radd__(self, number):
-		return self + number
+    def __radd__(self, number):
+        return self + number
 
-	def __sub__(self, number):
-		number = number if isinstance(number, Value) else Value(number)
-		out = Value(self.data - number.data, '-', children=[self, number])
-		return out
+    def __sub__(self, number):
+        number = number if isinstance(number, Value) else Value(number)
+        out = Value(self.data - number.data, '-', children=[self, number])
+        return out
 
-	def __rsub__(self, number):
-		number = number if isinstance(number, Value) else Value(number)
-		return number - self
+    def __rsub__(self, number):
+        number = number if isinstance(number, Value) else Value(number)
+        return number - self
 
-	def __mul__(self, number):
-		number = number if isinstance(number, Value) else Value(number)
-		out = Value(self.data * number.data, '*', children=[self, number])
-		return out
+    def __mul__(self, number):
+        number = number if isinstance(number, Value) else Value(number)
+        out = Value(self.data * number.data, '*', children=[self, number])
+        return out
 
-	def __rmul__(self, number):
-		return self * number
+    def __rmul__(self, number):
+        return self * number
 
-	def __pow__(self, number):
-		number = number if isinstance(number, Value) else Value(number)
-		out = Value(self.data ** number.data, '**', children=[self, number])
-		return out
+    def __pow__(self, number):
+        number = number if isinstance(number, Value) else Value(number)
+        out = Value(self.data ** number.data, '**', children=[self, number])
+        return out
 
-	def __gt__(self, number):
-		number = number if isinstance(number, Value) else Value(number)
-		if self.data > number.data:
-			return True
+    def __gt__(self, number):
+        number = number if isinstance(number, Value) else Value(number)
+        if self.data > number.data:
+            return True
 
-	def make_topo(self):
-		topo = []
-		visited = set()
-		stack = [self]
+    """
+    Build an array of all the nodes in such a way that
+    a node is added once all of it's parents are inside
+    the topo
+    """
+    def make_topo(self):
+        topo = []
+        visited = set()
+        stack = [self]
 
-		while stack:
-			node = stack.pop()
-			if node not in visited:
-				visited.add(node)
-				topo.append(node)
-				stack.extend(node.children)
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                continue
 
-		return topo
+            node_ready = True
+            for parent_node in node.parents:
+                if parent_node not in visited:
+                    node_ready = False
 
-	def update(self):
-		self.grad = 0
-		match self.op:
-			case '+':
-				self.data = self.children[0].data + self.children[1].data
-			case '-':
-				self.data = self.children[0].data - self.children[1].data
-			case '*':
-				self.data = self.children[0].data * self.children[1].data
-			case '**':
-				self.data = self.children[0].data ** self.children[1].data
-			case 'tanh':
-				self.data = np.tanh(self.children[0].data)
-	
-	# set the gradient of children
-	def backprop(self, topo):
-		self.grad = 1
+            if node_ready:
+                visited.add(node)
+                topo.append(node)
+                stack.extend(node.children)
 
-		for node in topo:
-			match node.op:
-				case '+':
-					node.children[0].grad += node.grad
-					node.children[1].grad += node.grad
-				case '*':
-					node.children[0].grad += node.children[1].data * node.grad
-					node.children[1].grad += node.children[0].data * node.grad
-				case 'tanh':
-					node.children[0].grad += (1 - node.data**2) * node.grad
-				case '-':
-					node.children[0].grad += node.grad
-					node.children[1].grad -= node.grad
-				case '**':
-					#TODO: make this expression shorter
-					node.children[0].grad += node.children[1].data * (node.children[0].data ** (node.children[1].data -1)) * node.grad
-		
-	def __repr__(self):
-		return f"data = {self.data}, gradient = {self.grad}, op = {self.op}"
+        return topo
+
+    def update(self):
+        self.grad = 0
+        match self.op:
+            case '+':
+                self.data = self.children[0].data + self.children[1].data
+            case '-':
+                self.data = self.children[0].data - self.children[1].data
+            case '*':
+                self.data = self.children[0].data * self.children[1].data
+            case '**':
+                self.data = self.children[0].data ** self.children[1].data
+            case 'tanh':
+                self.data = np.tanh(self.children[0].data)
+
+    # set the gradient of children
+    def backprop(self, topo):
+        self.grad = 1
+
+        for node in topo:
+            match node.op:
+                case '+':
+                    node.children[0].grad += node.grad
+                    node.children[1].grad += node.grad
+                case '*':
+                    node.children[0].grad += node.children[1].data * node.grad
+                    node.children[1].grad += node.children[0].data * node.grad
+                case 'tanh':
+                    node.children[0].grad += (1 - node.data**2) * node.grad
+                case '-':
+                    node.children[0].grad += node.grad
+                    node.children[1].grad -= node.grad
+                case '**':
+                    node.children[0].grad += node.children[1].data * (node.children[0].data ** (node.children[1].data -1)) * node.grad
+
+    def __repr__(self):
+        if not self.label:
+            return f"data = {self.data}, gradient = {self.grad}, op = {self.op}"
+        else:
+            return f"label = {self.label}, data = {self.data}, gradient = {self.grad}, op = {self.op}"

dumbgrad/nn.py
@@ -2,85 +2,85 @@ import numpy as np
 from dumbgrad.engine import Value
 
 class Neuron:
-	def __init__(self, number_inputs, number_outputs):
-		limit = np.sqrt(6 / (number_inputs + number_outputs))
-		self.w = [Value(np.random.uniform(-limit, limit), label='w') for _ in range(number_inputs)]
-		self.b = Value(0,label='b')
-	
-	def __call__(self, x):
-		act = sum((wi * xi) for (wi, xi) in zip(self.w, x)) + self.b
-		return act.tanh()
+    def __init__(self, number_inputs, number_outputs):
+        limit = np.sqrt(6 / (number_inputs + number_outputs))
+        self.w = [Value(np.random.uniform(-limit, limit), label='w') for _ in range(number_inputs)]
+        self.b = Value(0,label='b')
 
-	def parameters(self):
-		return self.w + [self.b]
+    def __call__(self, x):
+        # activation
+        act = sum((wi * xi) for (wi, xi) in zip(self.w, x)) + self.b
+        return act.tanh()
+
+    def parameters(self):
+        return self.w + [self.b]
 
 
 class Layer:
-	def __init__(self, number_inputs, number_outputs):
-		self.neurons = [Neuron(number_inputs, number_outputs) for _ in range(number_outputs)]
-	
-	def __call__(self, x):
-		out = [n(x) for n in self.neurons]
-		return out
+    def __init__(self, number_inputs, number_outputs):
+        self.neurons = [Neuron(number_inputs, number_outputs) for _ in range(number_outputs)]
 
-	def parameters(self):
-		return [p for n in self.neurons for p in n.parameters()]
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
 
 class Network:
-	def __init__(self, number_inputs, layer_sizes):
-		sz = [number_inputs] + layer_sizes
-		self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz)-1)]
-		
+    def __init__(self, number_inputs, layer_sizes):
+        sz = [number_inputs] + layer_sizes
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz)-1)]
 
-	def __call__(self, x):
-		for l in self.layers:
-			x = l(x)
-		return x
+    def __call__(self, x):
+        for l in self.layers:
+            out = l(x)
+        return out
 
-	def parameters(self):
-		return [p for l in self.layers for p in l.parameters()]
-	
-	def zero_grad(self):
-		for p in self.parameters():
-			p.grad = 0
+    def parameters(self):
+        return [p for l in self.layers for p in l.parameters()]
 
-	def train(self, inputs, outputs, omega1=0.9, omega2=0.99, lr=0.08, epochs=100, eps = 1e-6):
-		"""
-		 this builds the entire graph of the network
-		 and since i am calculating the loss for all
-		 of the inputs at once all that needs to be done
-		 is to just call update() for all of the nodes
-		 and then backprop
-		"""
-		y_pred = [self(x) for x in inputs]
-		diff = np.subtract(outputs, y_pred)
-		loss = np.sum(np.power(diff, 2))
-		topo = loss.make_topo()
-		t = 0
-		for _ in range(epochs):
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
 
-			print(loss)
-			loss.backprop(topo)
-			t += 1
-			for p in self.parameters():
-				p.m = omega1 * p.m + (1 - omega1) * p.grad
-				p.v = omega2 * p.v + (1 - omega2) * p.grad**2
+    def train(self, inputs, outputs, omega1=0.9, omega2=0.99, lr=0.08, epochs=100, eps = 1e-6):
+        """
+         this builds the entire graph of the network
+         and since i am calculating the loss for all
+         of the inputs at once all that needs to be done
+         is to just call update() for all of the nodes
+         and then backprop
+        """
+        y_pred = [self(x) for x in inputs]
+        diff = np.subtract(outputs, y_pred)
+        loss = np.sum(np.power(diff, 2))
+        topo = loss.make_topo()
+        t = 0
+        for _ in range(epochs):
 
-				m_hat = p.m / (1 - omega1 ** t)
-				v_hat = p.v / (1 - omega2 ** t)
+            print(loss)
+            loss.backprop(topo)
+            t += 1
+            for p in self.parameters():
+                p.m = omega1 * p.m + (1 - omega1) * p.grad
+                p.v = omega2 * p.v + (1 - omega2) * p.grad**2
 
-				p.data = p.data - lr * m_hat / (np.sqrt(v_hat) + eps)
-			for node in reversed(topo):
-				node.update()
-			
-		return loss
+                m_hat = p.m / (1 - omega1 ** t)
+                v_hat = p.v / (1 - omega2 ** t)
 
-	def test(self, inputs, outputs):
-		# this will need to be optimized
-		y_pred = [self(x) for x in inputs]
-		correct_count = 0
-		for pred, output in zip(y_pred, outputs):
-			if np.argmax(pred) == np.argmax(output):
-				correct_count += 1
+                p.data = p.data - lr * m_hat / (np.sqrt(v_hat) + eps)
+            for node in reversed(topo):
+                node.update()
 
-		print("accuracy: ", correct_count / len(outputs))
+        return loss
+
+    def test(self, inputs, outputs):
+        # this will need to be optimized
+        y_pred = [self(x) for x in inputs]
+        correct_count = 0
+        for pred, output in zip(y_pred, outputs):
+            if np.argmax(pred) == np.argmax(output):
+                correct_count += 1
+
+        print("accuracy: ", correct_count / len(outputs))

tests/digits.py
@@ -1,15 +1,21 @@
 from dumbgrad.engine import Value
 from dumbgrad.nn import Network
 import numpy as np
-from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
 
+def to_categorical(y, num_classes):
+    batch_size = len(y)
+    categorical = np.zeros((batch_size, num_classes))
+    for _i, _y in enumerate(y):
+        categorical[_i, _y] = 1
+    return categorical
+
 x, y = load_digits(return_X_y=True)
-x = x / 16		#normalize
+x = x / 16  #normalize
 num_classes = len(np.unique(y))
 y = to_categorical(y, num_classes=num_classes)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False) 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=False)
 
 n = Network(len(x[0]), [30, 30, num_classes])
 n.train(x_train, y_train, epochs=150, lr=0.01)