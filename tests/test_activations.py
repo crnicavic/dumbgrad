770e20c925517f955f075fa018233cbfc1d40f65
crnicavic <cavic5000@tutanota.com>
2026-07-13 14:50:25 +0200

    test: add testing for gradients of new activation functions

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -17,7 +17,7 @@ def act(x, activation):
     y = activation(x)
     return y
 
-def chained_act(x, activaiton):
+def chained_act(x, activation):
     if not isinstance(x, Value):
         x = Value(x)
 
@@ -26,7 +26,7 @@ def chained_act(x, activaiton):
         y = activation(y)
     return y
 
-def test_act():
+def test_sanity():
     """
     A rather pointless test that only serves as a quick sanity check
     might also come in handy if the internals change
@@ -48,3 +48,77 @@ def test_act():
     y = x.leaky_relu()
     y_act = act(x, Value.leaky_relu)
     assert y.data == y_act.data
+
+def test_act():
+    def tanh(x):
+        return act(x, Value.tanh)
+
+    def sigmoid(x):
+        return act(x, Value.sigmoid)
+
+    def relu(x):
+        return act(x, Value.relu)
+
+    def leaky_relu(x):
+        return act(x, Value.leaky_relu)
+
+    x = Value(0.5)
+    y = tanh(x)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(tanh, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+    x.grad = 0
+    y = sigmoid(x)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(sigmoid, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+    x.grad = 0
+    y = act(x, Value.relu)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(relu, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+    x.grad = 0
+    y = act(x, Value.leaky_relu)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(leaky_relu, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+def test_chained_act():
+    def tanh(x):
+        return chained_act(x, Value.tanh)
+
+    def sigmoid(x):
+        return chained_act(x, Value.sigmoid)
+
+    def relu(x):
+        return chained_act(x, Value.relu)
+
+    def leaky_relu(x):
+        return chained_act(x, Value.leaky_relu)
+
+    x = Value(0.5)
+    y = tanh(x)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(tanh, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+    x.grad = 0
+    y = sigmoid(x)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(sigmoid, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+    x.grad = 0
+    y = act(x, Value.relu)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(relu, x).data
+    assert abs(x.grad - numgrad) < 1e-3
+
+    x.grad = 0
+    y = act(x, Value.leaky_relu)
+    y.backprop(y.make_topo())
+    numgrad = numeric_grad(leaky_relu, x).data
+    assert abs(x.grad - numgrad) < 1e-3
